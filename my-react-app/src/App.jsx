135f803aad5751e2d70f48c703c09c434db16199
Bhumika Pandey <pandeybhumika267@gmail.com>
2025-01-09 22:09:48 +0530

    Ankit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,19 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './Pages/Home.jsx'
-import './App.css'
-
+import Home from './Pages/Home.jsx';
+import './App.css';
+import Preloader from './Components/Preloader.jsx';
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true);
+
+  // Simulate an API call or page load
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); // Hide preloader after 3 seconds (or your loading logic)
+    }, 3000);
+    return () => clearTimeout(timer); // Cleanup on component unmount
+  }, []);
 
   return (
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    {/* <Route path="/about" element={<About />} /> */}
-  </Routes>
-  </BrowserRouter>
-  )
+    <BrowserRouter>
+      {loading && (<Preloader/>)}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ my-react-app/src/Components/Preloader.jsx
@@ -0,0 +1,28 @@
+// Preloader.js
+import React from 'react';
+
+
+const Preloader = () => {
+  return (
+    <div className="preloader">
+          <div className="preloader-logo">
+            <a className="brand" href="#">
+              <img
+                className="brand-logo-dark"
+                src="https://icj24.com/wp-content/uploads/2024/08/ICJ24-Logo-.png"
+                alt=""
+                width="177"
+                height="59"
+              />
+            </a>
+          </div>
+          <div className="preloader-body">
+            <div className="cssload-container">
+              <div className="cssload-speeding-wheel"></div>
+            </div>
+          </div>
+        </div>
+  );
+};
+
+export default Preloader;
