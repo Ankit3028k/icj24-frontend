106a93eb8d2bd6aa7b7f5e610837358c8e38e5b4
Ankit Gangrade <ankitgangrade9617@gmail.com>
2025-01-26 12:36:07 +0530

    ankit

--- my-react-app/src/pages/RajnitiPage.jsx
+++ my-react-app/src/pages/RajnitiPage.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import NavBar from "../Components/NavBar";
 import Top from "../Components/top";
 import UpperNav from "../Components/UpperNav";
-import HeroSection from "../Components/RajnitiComponets/HeroSection";
+// import HeroSection from "../Components/RajnitiComponets/HeroSection";
 function Rajniti(){
     return(
         <div>
             <Top />
             <UpperNav />
             <NavBar />
-            <HeroSection />
+            {/* <HeroSection /> */}
         </div>
     )
 }
