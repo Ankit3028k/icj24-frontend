734a5b8f58a0d9beca8b8bc7b54899224edce81c
Ankit Gangrade <ankitgangrade9617@gmail.com>
2025-02-14 02:08:15 +0530

    ankit authentication  update

--- my-react-app/src/Components/ProtectedRoute.jsx
+++ my-react-app/src/Components/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 // ProtectedRoute component
 const ProtectedRoute = ({ requiredRole }) => {
   // Retrieve token and role from localStorage
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('icj24');
   const userRole = localStorage.getItem('role'); // Store role when user logs in
 
   // If no token or incorrect role, redirect to login page
