9c87594176fe5c3585013521f778ae2ba02de3de
Ankit Gangrade <ankitgangrade9617@gmail.com>
2025-02-14 16:22:47 +0530

    ankit change backend ip and timeout sec

--- my-react-app/src/Components/Admin/axiosConfig.js
+++ my-react-app/src/Components/Admin/axiosConfig.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://icj24-backend.vercel.app//api',  // Your API base URL here
-    timeout: 5000,                   // Timeout in ms
+    baseURL: 'https://icj24-backend.onrender.com/api',  // Your API base URL here
+    timeout: 10000,                   // Timeout in ms
     headers: {
         'Content-Type': 'application/json',
         // Add any other headers if needed (like Authorization)
